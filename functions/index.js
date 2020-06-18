b92b480ef590cc6e83b3fb34fb357166a45fc0cd
Renaud Lainé <rndlaine@gmail.com>
2020-06-18 15:57:56 -0400

    add firestore emulator

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
+const { db } = require('../index');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
-
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    let docRef = db.collection("records")
+    .add({ hello: "world" })
+    .then(() => response.send("Hello from Firebase!"))
+    .catch(() => response.status(500).send("Something went wrong!"))
 });
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // The Firebase Admin SDK to access Cloud Firestore.
 const admin = require('firebase-admin');
 admin.initializeApp();
+
+const db = admin.firestore();
+
+module.exports = {
+  db
+}
